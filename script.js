4c28d72ed968700ee59ecaeb1600dee808a7d2b4
Jordan Dulanto <ancajimadulantoj@gmail.com>
2026-09-22 18:29:38 -0500

    Add Rock Paper Scissors UI skeleton (HTML, CSS, element selectors)

--- script.js
+++ script.js
@@ -10,45 +10,8 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Choose between rock, paper, or scissors").toLowerCase()
-}
-
-
-function playGame() {
-
-    let humanScore = 0
-    let computerScore = 0
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            return "It's a tie!";
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            computerScore += 1
-            return "You Lose! Paper beats Rock";
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore += 1
-            return "You Win! Rock beats Scissors";
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore += 1
-            return "You Lose! Scissors beats Paper";
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore += 1
-            return "You Win! Paper beats Rock";
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            computerScore += 1
-            return "You Lose! Rock beats Scissors";
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore += 1
-            return "You Win! Scissors beats Paper";
-        }
-    }
-
-    for (let i = 0; i < 5; i += 1) {
-        console.log(playRound(getHumanChoice(), getComputerChoice()))
-    }
-
-    return `Final score: Human ${humanScore} - Computer ${computerScore}`
-}
-
-console.log(playGame())
+let rock = document.querySelector(".rock")
+let paper = document.querySelector(".paper")
+let scissors = document.querySelector(".scissors")
+let score = document.querySelector(".score")
+let message = document.querySelector(".message")
